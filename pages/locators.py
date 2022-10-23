be12b05d97665aafac52bbe0a643c6a450ba571e
ivanovajulika <julika77@mail.ru>
2022-10-23 19:17:22 +0700

    fixed XPATH

--- pages/locators.py
+++ pages/locators.py
@@ -4,15 +4,15 @@ class MainPageLocators():
 
     DOCUMENT_LINK = (By.XPATH, "//a[@href='/documentation']")
     SEARCH_BTN = (By.XPATH, '//*[@id="docsearch"]/button/span[1]/span')
-    WEBDRIVER_BTN = (By.XPATH, '/html/body/div/main/section[2]/div/div/div[1]/div/div[2]/div/a')
+    WEBDRIVER_BTN = (By.XPATH, 'a[href="/documentation/webdriver/"]')
     ABOUT_LINK = (By.XPATH, '//*[@id="main_navbar"]/ul/li[2]')
-    HISTORY_LINK = (By.XPATH, '//*[@id="main_navbar"]/ul/li[2]/div/a[5]')
+    HISTORY_LINK = (By.XPATH, '//*[@href="/history"]')
 
 class DocumPageLocators():
 
-    MENU_LINK = (By.XPATH, "//span[@class='td-sidebar-nav-active-item']")
+    MENU_LINK = (By.XPATH, '//*[@id="m-documentation"]/span')
     WEBDRIVER_LINK = (By.XPATH, '//*[@id="m-documentationwebdriver"]')
 
 class SearchLocators():
     OPEN_SEARCH = (By.XPATH, '//*[@id="docsearch-input"]')
-    ENTER_SEARCH = (By.CSS_SELECTOR, '#docsearch-item-0 > a > div > div.DocSearch-Hit-action > svg')
+    ENTER_SEARCH = (By.XPATH, '//*[@id="docsearch-item-0"]/a/div/div[3]')
