78543bee44148c8dc0d1c5d480ff27baee53cf75
juliadigrigorio <juliadigrigorio@hotmail.com>
2022-10-24 10:53:19 +0300

    locators fixed

--- pages/locators.py
+++ pages/locators.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
 
     DOCUMENT_LINK = (By.XPATH, "//a[@href='/documentation']")
-    SEARCH_BTN = (By.XPATH, '//*[@id="docsearch"]/button/span[1]/span')
-    WEBDRIVER_BTN = (By.XPATH, 'a[href="/documentation/webdriver/"]')
+    SEARCH_BTN = (By.XPATH, '//span[1]/span')
+    WEBDRIVER_BTN = (By.XPATH, '//div[1]/div/div[2]/div/a/i')
     ABOUT_LINK = (By.XPATH, '//*[@id="main_navbar"]/ul/li[2]')
-    HISTORY_LINK = (By.XPATH, '//*[@href="/history"]')
+    HISTORY_LINK = (By.XPATH, '//div/a[5]')
 
 class DocumPageLocators():
 
