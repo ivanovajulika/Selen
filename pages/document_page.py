27dc64807b1179184cf2aab48b4a544bdfc355dc
ivanovajulika <julika77@mail.ru>
2022-11-04 10:37:38 +0700

    updated yml

--- pages/document_page.py
+++ pages/document_page.py
@@ -3,13 +3,12 @@ from .base_page import BasePage
 
 
 class DocumentPage(BasePage):
-
     def should_be_document_page(self):
         self.should_be_document_link()
         self.should_be_menu_document()
 
     def should_be_document_link(self):
-        assert 'documentation' in self.browser.current_url, 'wrong url'
+        assert "documentation" in self.browser.current_url, "wrong url"
 
     def should_be_menu_document(self):
         assert self.element_is_present(*DocumPageLocators.MENU_LINK)
--- pages/webdriver_page.py
+++ pages/webdriver_page.py
@@ -2,6 +2,5 @@ from .base_page import BasePage
 
 
 class WebdriverPage(BasePage):
-
     def should_be_webdriver_link(self):
-        assert 'webdriver' in self.browser.current_url, 'wrong url'
+        assert "webdriver" in self.browser.current_url, "wrong url"
